d498cb55793188f28d0722924e67d5ef921c67c5
Эмиль Ахмадеев <r_he_lol@mail.ru>
2020-06-25 19:59:32 +0500

    start

---
+++ RasklKlavi/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace RasklKlavi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Приложение свернуто";
+            this.notifyIcon1.BalloonTipTitle = "Имя приложения";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "RasiK";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.закрытьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(121, 26);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "RasiK";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+    }
+}
+
---
+++ RasklKlavi/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RasklKlavi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+
+        globalKeyboardHook gkh = new globalKeyboardHook();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //добавляем какие будут горячие клавиши
+            gkh.HookedKeys.Add(Keys.Q);
+            gkh.HookedKeys.Add(Keys.W);
+            gkh.HookedKeys.Add(Keys.E);
+            gkh.HookedKeys.Add(Keys.R);
+            gkh.HookedKeys.Add(Keys.T);
+            gkh.HookedKeys.Add(Keys.Y);
+            gkh.HookedKeys.Add(Keys.U);
+            gkh.HookedKeys.Add(Keys.I);
+            gkh.HookedKeys.Add(Keys.O);
+            gkh.HookedKeys.Add(Keys.P);
+            gkh.HookedKeys.Add(Keys.Oem4);
+            gkh.HookedKeys.Add(Keys.Oem6);
+            gkh.HookedKeys.Add(Keys.A);
+            gkh.HookedKeys.Add(Keys.S);
+            gkh.HookedKeys.Add(Keys.D);
+            gkh.HookedKeys.Add(Keys.F);
+            gkh.HookedKeys.Add(Keys.G);
+            gkh.HookedKeys.Add(Keys.H);
+            gkh.HookedKeys.Add(Keys.J);
+            gkh.HookedKeys.Add(Keys.K);
+            gkh.HookedKeys.Add(Keys.L);
+            gkh.HookedKeys.Add(Keys.Oem1);
+            gkh.HookedKeys.Add(Keys.Oem7);
+            gkh.HookedKeys.Add(Keys.Z);
+            gkh.HookedKeys.Add(Keys.X);
+            gkh.HookedKeys.Add(Keys.C);
+            gkh.HookedKeys.Add(Keys.V);
+            gkh.HookedKeys.Add(Keys.B);
+            gkh.HookedKeys.Add(Keys.N);
+            gkh.HookedKeys.Add(Keys.M);
+            gkh.HookedKeys.Add(Keys.Oemcomma);
+            gkh.HookedKeys.Add(Keys.OemPeriod);
+            gkh.HookedKeys.Add(Keys.Oem2);
+            gkh.HookedKeys.Add(Keys.Space);
+            //
+            // и т.п.
+            gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+        }
+            
+        void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Tab)
+            {
+                //Если в буфере обмен содержится текст
+                if (Clipboard.ContainsText() == true)
+                {
+                    //Извлекаем (точнее копируем) его и сохраняем в переменную
+                    string someText = Clipboard.GetText();
+
+                    //Выводим показываем сообщение с текстом, скопированным из буфера обмена
+                    MessageBox.Show(this, someText, "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+            }
+        }
+
+
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            { 
+                notifyIcon1.Visible = false; 
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            //notifyIcon1.Visible = true;
+            //this.Hide();
+        }
+    }
+}
